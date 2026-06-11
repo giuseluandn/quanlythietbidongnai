d002264e05b5349f9b53445fbcbc79492a185723
luan nguyen <giuseluandn@gmail.com>
2026-06-11 17:40:06 +0700

    Update lay_thong_tin.bat

--- lay_thong_tin.bat
+++ lay_thong_tin.bat
@@ -14,7 +14,7 @@ chcp 65001 >nul 2>&1
 :: ─────────────────────────────────────────────────────────────────────────────
 
 powershell -NoProfile -ExecutionPolicy Bypass -Command ^
- "$u='https://nguyennam90.github.io/Check_thiet_bi';" ^
+ "$u='https://giuseluandn.github.io/quanlythietbidongnai';" ^
  "try{" ^
  "  $d=Join-Path $env:LOCALAPPDATA 'VNPost';" ^
  "  New-Item -ItemType Directory -Path $d -Force|Out-Null;" ^
